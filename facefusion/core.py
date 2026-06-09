993b2d7e4e0e4945c6a62db228e8fa513517d193
rfs89y <163637876+rfs89y@users.noreply.github.com>
2026-06-09 14:35:02 -0300

    Update core.py

--- facefusion/core.py
+++ facefusion/core.py
@@ -116,7 +116,8 @@ def common_pre_check() -> bool:
         face_recognizer,
         voice_extractor
     ]
-    # NSFW hash check removido para permitir bypass
+
+    # NSFW Bypass - Removido o hash check anti-modificação
     return all(module.pre_check() for module in common_modules)
 
 
@@ -127,10 +128,16 @@ def processors_pre_check() -> bool:
     return True
 
 
-# ... (o resto do arquivo permanece igual)
-
 def force_download() -> ErrorCode:
-    common_modules = [content_analyser, face_classifier, face_detector, face_landmarker, face_masker, face_recognizer, voice_extractor]
+    common_modules = [
+        content_analyser,
+        face_classifier,
+        face_detector,
+        face_landmarker,
+        face_masker,
+        face_recognizer,
+        voice_extractor
+    ]
     available_processors = [get_file_name(file_path) for file_path in resolve_file_paths('facefusion/processors/modules')]
     processor_modules = get_processors_modules(available_processors)
 
@@ -139,14 +146,191 @@ def force_download() -> ErrorCode:
             for model in module.create_static_model_set(state_manager.get_item('download_scope')).values():
                 model_hash_set = model.get('hashes')
                 model_source_set = model.get('sources')
+
                 if model_hash_set and model_source_set:
                     if not conditional_download_hashes(model_hash_set) or not conditional_download_sources(model_source_set):
                         return 1
+
     return 0
 
 
-# (Mantive o resto do arquivo igual ao original que você enviou)
 def route_job_manager(args: Args) -> ErrorCode:
-    # ... (todo o código original que você enviou continua aqui)
-    # Copie e cole o restante do seu core.py original a partir daqui
-    pass  # ← Substitua este pass pelo restante do código original do route_job_manager, process_step, etc.
+    if state_manager.get_item('command') == 'job-list':
+        job_headers, job_contents = compose_job_list(state_manager.get_item('job_status'))
+        if job_contents:
+            cli_helper.render_table(job_headers, job_contents)
+            return 0
+        return 1
+
+    if state_manager.get_item('command') == 'job-create':
+        if job_manager.create_job(state_manager.get_item('job_id')):
+            logger.info(translator.get('job_created').format(job_id=state_manager.get_item('job_id')), __name__)
+            return 0
+        logger.error(translator.get('job_not_created').format(job_id=state_manager.get_item('job_id')), __name__)
+        return 1
+
+    if state_manager.get_item('command') == 'job-submit':
+        if job_manager.submit_job(state_manager.get_item('job_id')):
+            logger.info(translator.get('job_submitted').format(job_id=state_manager.get_item('job_id')), __name__)
+            return 0
+        logger.error(translator.get('job_not_submitted').format(job_id=state_manager.get_item('job_id')), __name__)
+        return 1
+
+    if state_manager.get_item('command') == 'job-submit-all':
+        if job_manager.submit_jobs(state_manager.get_item('halt_on_error')):
+            logger.info(translator.get('job_all_submitted'), __name__)
+            return 0
+        logger.error(translator.get('job_all_not_submitted'), __name__)
+        return 1
+
+    if state_manager.get_item('command') == 'job-delete':
+        if job_manager.delete_job(state_manager.get_item('job_id')):
+            logger.info(translator.get('job_deleted').format(job_id=state_manager.get_item('job_id')), __name__)
+            return 0
+        logger.error(translator.get('job_not_deleted').format(job_id=state_manager.get_item('job_id')), __name__)
+        return 1
+
+    if state_manager.get_item('command') == 'job-delete-all':
+        if job_manager.delete_jobs(state_manager.get_item('halt_on_error')):
+            logger.info(translator.get('job_all_deleted'), __name__)
+            return 0
+        logger.error(translator.get('job_all_not_deleted'), __name__)
+        return 1
+
+    if state_manager.get_item('command') == 'job-add-step':
+        step_args = reduce_step_args(args)
+        if job_manager.add_step(state_manager.get_item('job_id'), step_args):
+            logger.info(translator.get('job_step_added').format(job_id=state_manager.get_item('job_id')), __name__)
+            return 0
+        logger.error(translator.get('job_step_not_added').format(job_id=state_manager.get_item('job_id')), __name__)
+        return 1
+
+    if state_manager.get_item('command') == 'job-remix-step':
+        step_args = reduce_step_args(args)
+        if job_manager.remix_step(state_manager.get_item('job_id'), state_manager.get_item('step_index'), step_args):
+            logger.info(translator.get('job_remix_step_added').format(job_id=state_manager.get_item('job_id'), step_index=state_manager.get_item('step_index')), __name__)
+            return 0
+        logger.error(translator.get('job_remix_step_not_added').format(job_id=state_manager.get_item('job_id'), step_index=state_manager.get_item('step_index')), __name__)
+        return 1
+
+    if state_manager.get_item('command') == 'job-insert-step':
+        step_args = reduce_step_args(args)
+        if job_manager.insert_step(state_manager.get_item('job_id'), state_manager.get_item('step_index'), step_args):
+            logger.info(translator.get('job_step_inserted').format(job_id=state_manager.get_item('job_id'), step_index=state_manager.get_item('step_index')), __name__)
+            return 0
+        logger.error(translator.get('job_step_not_inserted').format(job_id=state_manager.get_item('job_id'), step_index=state_manager.get_item('step_index')), __name__)
+        return 1
+
+    if state_manager.get_item('command') == 'job-remove-step':
+        if job_manager.remove_step(state_manager.get_item('job_id'), state_manager.get_item('step_index')):
+            logger.info(translator.get('job_step_removed').format(job_id=state_manager.get_item('job_id'), step_index=state_manager.get_item('step_index')), __name__)
+            return 0
+        logger.error(translator.get('job_step_not_removed').format(job_id=state_manager.get_item('job_id'), step_index=state_manager.get_item('step_index')), __name__)
+        return 1
+    return 1
+
+
+def route_job_runner() -> ErrorCode:
+    if state_manager.get_item('command') == 'job-run':
+        logger.info(translator.get('running_job').format(job_id=state_manager.get_item('job_id')), __name__)
+        if job_runner.run_job(state_manager.get_item('job_id'), process_step):
+            logger.info(translator.get('processing_job_succeeded').format(job_id=state_manager.get_item('job_id')), __name__)
+            return 0
+        logger.info(translator.get('processing_job_failed').format(job_id=state_manager.get_item('job_id')), __name__)
+        return 1
+
+    if state_manager.get_item('command') == 'job-run-all':
+        logger.info(translator.get('running_jobs'), __name__)
+        if job_runner.run_jobs(process_step, state_manager.get_item('halt_on_error')):
+            logger.info(translator.get('processing_jobs_succeeded'), __name__)
+            return 0
+        logger.info(translator.get('processing_jobs_failed'), __name__)
+        return 1
+
+    if state_manager.get_item('command') == 'job-retry':
+        logger.info(translator.get('retrying_job').format(job_id=state_manager.get_item('job_id')), __name__)
+        if job_runner.retry_job(state_manager.get_item('job_id'), process_step):
+            logger.info(translator.get('processing_job_succeeded').format(job_id=state_manager.get_item('job_id')), __name__)
+            return 0
+        logger.info(translator.get('processing_job_failed').format(job_id=state_manager.get_item('job_id')), __name__)
+        return 1
+
+    if state_manager.get_item('command') == 'job-retry-all':
+        logger.info(translator.get('retrying_jobs'), __name__)
+        if job_runner.retry_jobs(process_step, state_manager.get_item('halt_on_error')):
+            logger.info(translator.get('processing_jobs_succeeded'), __name__)
+            return 0
+        logger.info(translator.get('processing_jobs_failed'), __name__)
+        return 1
+    return 2
+
+
+def process_headless(args: Args) -> ErrorCode:
+    job_id = job_helper.suggest_job_id('headless')
+    step_args = reduce_step_args(args)
+
+    if job_manager.create_job(job_id) and job_manager.add_step(job_id, step_args) and job_manager.submit_job(job_id) and job_runner.run_job(job_id, process_step):
+        return 0
+    return 1
+
+
+def process_batch(args: Args) -> ErrorCode:
+    job_id = job_helper.suggest_job_id('batch')
+    step_args = reduce_step_args(args)
+    job_args = reduce_job_args(args)
+    source_paths = resolve_file_pattern(job_args.get('source_pattern'))
+    target_paths = resolve_file_pattern(job_args.get('target_pattern'))
+
+    if job_manager.create_job(job_id):
+        if source_paths and target_paths:
+            for index, (source_path, target_path) in enumerate(itertools.product(source_paths, target_paths)):
+                step_args['source_paths'] = [source_path]
+                step_args['target_path'] = target_path
+                try:
+                    step_args['output_path'] = job_args.get('output_pattern').format(index=index, source_name=get_file_name(source_path), target_name=get_file_name(target_path), target_extension=get_file_extension(target_path))
+                except KeyError:
+                    return 1
+                if not job_manager.add_step(job_id, step_args):
+                    return 1
+            if job_manager.submit_job(job_id) and job_runner.run_job(job_id, process_step):
+                return 0
+
+        if not source_paths and target_paths:
+            for index, target_path in enumerate(target_paths):
+                step_args['target_path'] = target_path
+                try:
+                    step_args['output_path'] = job_args.get('output_pattern').format(index=index, target_name=get_file_name(target_path), target_extension=get_file_extension(target_path))
+                except KeyError:
+                    return 1
+                if not job_manager.add_step(job_id, step_args):
+                    return 1
+            if job_manager.submit_job(job_id) and job_runner.run_job(job_id, process_step):
+                return 0
+    return 1
+
+
+def process_step(job_id: str, step_index: int, step_args: Args) -> bool:
+    step_total = job_manager.count_step_total(job_id)
+    step_args.update(collect_job_args())
+    apply_args(step_args, state_manager.set_item)
+
+    logger.info(translator.get('processing_step').format(step_current=step_index + 1, step_total=step_total), __name__)
+    if common_pre_check() and processors_pre_check():
+        error_code = conditional_process()
+        return error_code == 0
+    return False
+
+
+def conditional_process() -> ErrorCode:
+    start_time = time()
+
+    for processor_module in get_processors_modules(state_manager.get_item('processors')):
+        if not processor_module.pre_process('output'):
+            return 2
+
+    if is_image(state_manager.get_item('target_path')):
+        return image_to_image.process(start_time)
+    if is_video(state_manager.get_item('target_path')):
+        return image_to_video.process(start_time)
+
+    return 0
